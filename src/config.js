d8f63b4a88b03aca1c7b7e8fcc7b91d974c4a1f1
dongwon <kkum9408@gmail.com>
2021-08-18 02:49:02 +0900

    Refactor: Timer 함수 utils 분리

--- src/config.js
+++ src/config.js
@@ -1,3 +1,39 @@
 export const ROUTES = {
   HOME: "/",
 };
+
+// prettier-ignore
+export const DATE_STR = {
+  daysKR: [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ],
+  daysEN: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  MonthsEN: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,19 @@
 import { quickSort } from "./quickSort";
 import { getNumsArray } from "./getNumsArray";
 import { inputValidate } from "./inputValidate";
+import { getCurTime } from "./getCurTime";
+import { getFormTime } from "./getFormTime";
+import { getFormDateKR } from "./getFormDateKR";
+import { getFormDateUS } from "./getFormDateUS";
+import { getCurDate } from "./getCurDate";
 
-export { quickSort, getNumsArray, inputValidate };
+export {
+  quickSort,
+  getNumsArray,
+  inputValidate,
+  getCurTime,
+  getFormTime,
+  getFormDateKR,
+  getFormDateUS,
+  getCurDate,
+};
